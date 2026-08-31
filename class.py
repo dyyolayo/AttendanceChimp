b71e66ec4bbf59ec98042da892d448f26aa0d16b
dyyolayo <oeo@uchicago.edu>
2026-08-30 23:22:50 -0400

    Created classes for most of the objects for DB

--- class.py
+++ class.py
@@ -1,22 +1,47 @@
 # Blueprint for all the people in the classroom
 
 ## This comment, I hope to use when I have something I want to add vs the regular # comment 
-### Comment for personal reading, not to be pushed
+### Personal comments
 
 # Everytime I add a student to a course, I want the course amount to update
 
-# Each student has a first name, a last name, and a course 
-## 
+############TODO############
+#Look through code and note necessary edits here 
+## Look into DB design to verify if a teacher is in a course, start working on SQL
+## Might want to change the design so that 
+## Separate helper functions from here
 
+daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+def verify_DOW(day:str):
+    """This function is meant to verify if a proposed class date is one of the 
+    days of the week"""
+
+    try:
+        if day in daysOfTheWeek:
+            return True
+        
+        raise ValueError(
+            f"The day you entered, {day} is not one of the available days of the week"
+                         ) #replace with logs
+    
+    except ValueError:
+        return False
+    
+    except TypeError:
+        raise TypeError (
+            "Day must be a string"
+            )
 
 class Student:
-
+    # each student may take multiple courses, many students in each course
     def __init__(self,first:str,last:str):
         self.first = first
         self.last = last 
         self.courses = [] ###read python doc, here we are 
 
     def add_course(self,*course):
+        """Adds a course to the list of courses for each individual"""
         for courseItem in course:
             self.courses.append(courseItem)
         ## Make it so that there is a clickable that allows the person to add the course through JS and then populate it like that
@@ -25,13 +50,48 @@ class Student:
         return f"{self.first} {self.last}"
 
 class Teacher:
-    def __init__(self,first,last,courses):
+    # each teacher may teach multiple courses, there can be multiple teachers for a course
+    def __init__(self,first,last):
         self.first = first
         self.last = last
-        self.courses = courses #each teacher teaches at least one course
+        self.courses = [] #each teacher teaches at least one course
+
+    def add_course(self,course):
+        """Adds a course to the list of courses for each individual"""
+        for courseItem in course:
+            self.courses.append(courseItem)
+
+class Course:
+    ##Want to add a way to map each lecture time to the specific lecture days, i.e. MWF : 10-1, T,Th: 3-4
+    # each Course can have multiple teachers, multiple enrolled and multiple lecture dates and times
+    def __init__(self,subject:str,name:str,numLectures:int):
+        self.subject = subject
+        self.name = name
+        self.numLectures = numLectures
+        self.lectureDateTimes = {}
+
+    def add_Lecture_DateTime(self,date,time):
+        if verify_DOW(date):
+            self.lectureDateTimes[date] = time
+        # Check format for date addition is correct
+        # condition for if it doesn't exist later
+
+class Lecture:
+    def __init__(self,course,time):
+        self.course = course 
+        self.time = time
+        self.teachers = []
+
+    # def add_teacher(self,teacher):
 
 
-firstStudent = Student("Jack","Swagger")
-firstStudent.add_course("Math","Science")
-print(firstStudent.full_name())
-print(firstStudent.courses)
+Math100 = Course("Math","Math100",2)
+
+Math100.add_Lecture_DateTime("Monday","7:30")
+
+print(Math100.lectureDateTimes)
+
+# firstStudent = Student("Jack","Swagger")
+# firstStudent.add_course("Math","Science")
+# print(firstStudent.full_name())
+# print(firstStudent.courses)
